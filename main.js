256abf0fa30f1917f30f7b9102c6933395924d69
Chris-dagostino <dagostino.christian94@gmail.com>
2022-02-17 17:02:55 +0100

    lista 10 email

--- main.js
+++ main.js
@@ -3,15 +3,21 @@ const app = new Vue (
         el:'#app',
         data:{
             arrayEmail:[],
+            
         },
+
 
         mounted(){
 
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then((response) => {
-                    this.arrayEmail = response.data.response;
-                }
-            );
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((result) => {
+                        this.arrayEmail.push(result.data.response);
+                        
+                    }
+    
+                );
+          }
         }
     }
 );
